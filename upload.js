864648d2c91c7c7b3287ce14c40460eeb3d8e0d7
averyawesome555 <32853959+averyawesome555@users.noreply.github.com>
2019-12-23 16:31:17 -0500

    Update upload.js

--- upload.js
+++ upload.js
@@ -3,12 +3,11 @@ $(document).ready(function(){
 
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get('code');
-    const redirect_uri = "https://backupbinder.netlify.com/upload.html" // replace with your redirect_uri;
-    const client_secret = "RB2ey8CIoVUzAWaHsk8me158"; // replace with your client secret
+    const redirect_uri = "" // replace with your redirect_uri;
+    const client_secret = ""; // replace with your client secret
     const scope = "https://www.googleapis.com/auth/drive";
     var access_token= "";
-    var client_id = "968027827531-mb60n4enbs2a8enarbj219qs816o444r.apps.googleusercontent.com"// replace it with your client id;
-    var upload
+    var client_id = ""// replace it with your client id;
     
 
     $.ajax({
@@ -39,73 +38,36 @@ $(document).ready(function(){
         return url.split("?")[0].split("#")[0];
     }   
 
-    var Upload = function (file) {
-        this.file = file;
-    };
-    
-    Upload.prototype.getType = function() {
-        localStorage.setItem("type",this.file.type);
-        return this.file.type;
-    };
-    Upload.prototype.getSize = function() {
-        localStorage.setItem("size",this.file.size);
-        return this.file.size;
-    };
-    Upload.prototype.getName = function() {
-        return this.file.name;
-    };
-    Upload.prototype.doUpload = function () {
-      var metadata = {
-        'name': this.getName()
-      };
-
-      var accessToken = localStorage.getItem("accessToken"); // Here gapi is used for retrieving the access token.
-      var form = new FormData();
-      form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
-      form.append('file', this.file);
-
-      var xhr = new XMLHttpRequest();
-      xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id');
-      xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
-      xhr.setRequestHeader('Content-Type', this.getType());
-      xhr.setRequestHeader('Content-Length', this.getSize())
-      xhr.responseType = 'json';
-      xhr.onload = () => {
-          console.log(xhr.responseText); // Retrieve uploaded file ID.
-      };
-      xhr.send(form);
-    };
-    
-    Upload.prototype.progressHandling = function (event) {
-        var percent = 0;
-        var position = event.loaded || event.position;
-        var total = event.total;
-        var progress_bar_id = "#progress-wrp";
-        if (event.lengthComputable) {
-            percent = Math.ceil(position / total * 100);
-        }
-        // update progressbars classes so it fits your code
-        $(progress_bar_id + " .progress-bar").css("width", +percent + "%");
-        $(progress_bar_id + " .status").text(percent + "%");
-    };
 
     $("#upload").on("click", function (e) {
-        var file = $("#files")[0].files[0];
-        upload = new Upload(file);
-    
-        // maby check size or type here with upload.getSize() and upload.getType()
-    
-        // execute upload
-        upload.doUpload();
+        //var file = $("#files")[0].files[0];
+
+
+        var fileContent = 'sample text'; // As a sample, upload a text file.
+        var file = new Blob([fileContent], {type: 'text/plain'});
+        var metadata = {
+            'name': 'sampleName', // Filename at Google Drive
+            'mimeType': 'text/plain', // mimeType at Google Drive
+            'parents': ['### folder ID ###'], // Folder ID at Google Drive
+        };
+
+        var accessToken = localStorage.getItem("accessToken"); // Here gapi is used for retrieving the access token.
+        var form = new FormData();
+        form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
+        form.append('file', file);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id');
+        xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken);
+        xhr.responseType = 'json';
+        xhr.onload = () => {
+            console.log(xhr.response.id); // Retrieve uploaded file ID.
+        };
+        xhr.send(form);
+
     });
 
 
 
     
 });
-
-
-
-
-
-
