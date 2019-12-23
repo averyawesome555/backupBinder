cdd463566a2caed140b77fceb784ed073838d485
averyawesome555 <32853959+averyawesome555@users.noreply.github.com>
2019-12-23 11:49:18 -0500

    Update upload.js

--- upload.js
+++ upload.js
@@ -66,7 +66,7 @@ $(document).ready(function(){
       form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
       form.append('file', this.file);
 
-      fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id', {
+      fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=media', {
           method: 'POST',
           headers: new Headers({ 'Authorization': 'Bearer ' + accessToken }),
           body: form,
