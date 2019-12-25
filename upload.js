b65f2b38e70e437a35059b7637e4e620592b1f39
averyawesome555 <32853959+averyawesome555@users.noreply.github.com>
2019-12-25 11:50:04 -0500

    Update upload.js

--- upload.js
+++ upload.js
@@ -37,32 +37,17 @@ $(document).ready(function(){
         return url.split("?")[0].split("#")[0];
     }   
 
-    var Upload = function (file) {
-        this.file = file;
-    };
-    
-    Upload.prototype.getType = function() {
-        localStorage.setItem("type",this.file.type);
-        return this.file.type;
-    };
-    Upload.prototype.getSize = function() {
-        localStorage.setItem("size",this.file.size);
-        return this.file.size;
-    };
-    Upload.prototype.getName = function() {
-        return this.file.name;
-    };
-    Upload.prototype.doUpload = function () {
-        var that = this;
+    function uploadFile(File file) {
+//        var that = this;
         var formData = new FormData();
         var metadata = {
-            'name': this.getName(), // Filename at Google Drive
-            'mimeType': this.getType(), // mimeType at Google Drive
+            'name': file.getName(), // Filename at Google Drive
+            'mimeType': file.getType(), // mimeType at Google Drive
         };
     
         // add assoc key values, this will be posts values
         formData.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
-        formData.append("file", this.file);
+        formData.append("file", file);
     
         $.ajax({
             type: "POST",
@@ -71,17 +56,12 @@ $(document).ready(function(){
                 
             },
             url: "https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id",
-            xhr: function () {
-                var myXhr = $.ajaxSettings.xhr();
-                if (myXhr.upload) {
-                    myXhr.upload.addEventListener('progress', that.progressHandling, false);
-                }
-                return myXhr;
-            },
             success: function (data) {
+                console.log("Succsesful upload?")
                 console.log(data);
             },
             error: function (error) {
+                console.log("LLLLLLLL")
                 console.log(error);
             },
             async: true,
@@ -93,27 +73,22 @@ $(document).ready(function(){
         });
     };
     
-    Upload.prototype.progressHandling = function (event) {
-        var percent = 0;
-        var position = event.loaded || event.position;
-        var total = event.total;
-        var progress_bar_id = "#progress-wrp";
-        if (event.lengthComputable) {
-            percent = Math.ceil(position / total * 100);
-        }
-        // update progressbars classes so it fits your code
-        $(progress_bar_id + " .progress-bar").css("width", +percent + "%");
-        $(progress_bar_id + " .status").text(percent + "%");
-    };
+//    Upload.prototype.progressHandling = function (event) {
+//        var percent = 0;
+//        var position = event.loaded || event.position;
+//        var total = event.total;
+//        var progress_bar_id = "#progress-wrp";
+//        if (event.lengthComputable) {
+//            percent = Math.ceil(position / total * 100);
+//        }
+//        // update progressbars classes so it fits your code
+//        $(progress_bar_id + " .progress-bar").css("width", +percent + "%");
+//        $(progress_bar_id + " .status").text(percent + "%");
+//    };
 
     $("#upload").on("click", function (e) {
         var file = $("#files")[0].files[0];
-        var upload = new Upload(file);
-    
-        // maby check size or type here with upload.getSize() and upload.getType()
-    
-        // execute upload
-        upload.doUpload();
+        uploadFile(file);
     });
 
 
