9a627dd8150f054041d8e3684a48bc943913e352
averyawesome555 <32853959+averyawesome555@users.noreply.github.com>
2020-01-20 10:44:52 -0500

    minor fix

--- upload.js
+++ upload.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
            //window.history.pushState({}, document.title, "/GitLoginApp/" + "upload.html");
 
            isFirstTimeLogin();
-           getChildren("Backup Binder");
+           listAll();
         }
   });
 
