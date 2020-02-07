ebef75b738d1918e97a0e857001718e06a561747
LightScale-Apps <ryanbhillis@gmail.com>
2020-02-07 13:44:41 +0000

    debuggihng

--- main.js
+++ main.js
@@ -313,6 +313,8 @@ $(document).ready(function(){
           }).catch(function(error) {console.log(error)});
         } // end of for-loop
         var result = $.parseJSON($("#binderContent").text());
+		console.log("RESULT:")
+		console.log(result)
         for (var key in result) {
 
 		  $("key").innerHTML = key;
