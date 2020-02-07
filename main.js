0f59eb36b56a64e2d196bfe51d8ac73b03737874
LightScale-Apps <ryanbhillis@gmail.com>
2020-02-07 23:49:30 +0000

    werwerwer

--- main.js
+++ main.js
@@ -1,40 +1,4 @@
-
-
-
-
-$(document).ready(function(){
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get("code");
-    const redirect_uri = "https://backupbinder.netlify.com/reminders.html" // replace with your redirect_uri;
-    const client_secret = "vVkHKV4hswLANEHqI-CYCzX3"; // replace with your client secret
-    const scope = "https://www.googleapis.com/auth/drive.file";
-    var access_token= "";
-    const client_id = "146136756337-jt4b3n285gl57vthk47jtdq18nlib6rh.apps.googleusercontent.com"; // replace it with your client id
-
-    $.ajax({
-        type: "POST",
-        url: "https://www.googleapis.com/oauth2/v4/token",
-        data: {code:code
-            ,redirect_uri:redirect_uri,
-            client_secret:client_secret,
-        client_id:client_id,
-        scope:scope,
-        grant_type:"authorization_code"},
-        dataType: "json",
-        success: function(resultData) {
-
-           console.log("Result Data: ", resultData);
-           localStorage.setItem("accessToken",resultData.access_token);
-           localStorage.setItem("refreshToken",resultData.refreshToken);
-           localStorage.setItem("expires_in",resultData.expires_in);
-           //window.history.pushState({}, document.title, "/GitLoginApp/" + "upload.html");
-
-           isFirstTimeLogin();
-        }
-  });
-  // hey guys, this is austin
-
-    $("#uploadFile").on("click", function (e) {
+$("#uploadFile").on("click", function (e) {
 		$("#files").click();
 		$("#uploadModal").modal();
 //        var file = $("#files")[0].files[0];
@@ -103,8 +67,6 @@ $(document).ready(function(){
 
       }); // end of .catch
     }
-
-	window["uploadFileFunction"] = uploadFileFunction
 
     function createFolder(folderName) {
         getItemID("Backup Binder").then(function(result) {
@@ -436,6 +398,42 @@ $(document).ready(function(){
             timeout: 60000
         });
     }
+
+
+
+$(document).ready(function(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+    const redirect_uri = "https://backupbinder.netlify.com/reminders.html" // replace with your redirect_uri;
+    const client_secret = "vVkHKV4hswLANEHqI-CYCzX3"; // replace with your client secret
+    const scope = "https://www.googleapis.com/auth/drive.file";
+    var access_token= "";
+    const client_id = "146136756337-jt4b3n285gl57vthk47jtdq18nlib6rh.apps.googleusercontent.com"; // replace it with your client id
+
+    $.ajax({
+        type: "POST",
+        url: "https://www.googleapis.com/oauth2/v4/token",
+        data: {code:code
+            ,redirect_uri:redirect_uri,
+            client_secret:client_secret,
+        client_id:client_id,
+        scope:scope,
+        grant_type:"authorization_code"},
+        dataType: "json",
+        success: function(resultData) {
+
+           console.log("Result Data: ", resultData);
+           localStorage.setItem("accessToken",resultData.access_token);
+           localStorage.setItem("refreshToken",resultData.refreshToken);
+           localStorage.setItem("expires_in",resultData.expires_in);
+           //window.history.pushState({}, document.title, "/GitLoginApp/" + "upload.html");
+
+           isFirstTimeLogin();
+        }
+  })
+  // hey guys, this is austin
+
+    
 
 	listAll()
 
